0ccb553ada451fb54908293536d963bd2ce85a71
Yusuke YONEMURA <detomasomodena@gmail.com>
2019-04-26 18:11:43 +0900

    Update 00.py

--- ch1/00.py
+++ ch1/00.py
@@ -1,19 +1,46 @@
 
+""" 言語処理100本ノック 2015 第1章: 準備運動
+00. 文字列の逆順
+文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ。
+"""
+
 def reverse(arg):
- length = len(arg)
- reverse = ''
- while length != 0:
-  reverse += s[length-1]
-  length -= 1
- return reverse
+    """ 引数に指定された文字列を逆に並べる。
+
+    Args:
+      arg: （末尾から先頭に向かって）逆に並べる文字列
+    Raise:
+      特になし
+    Return:
+      （末尾から先頭に向かって）逆に並べられた文字列
+    """
+    length = len(arg)
+    reverse = ''
+    while length != 0:
+        reverse += s[length-1]
+        length -= 1
+    return reverse
 
 def lreverse(arg):
- larg = list(arg)
- larg.reverse()
- reverse = ''
- for e in larg:
-  reverse += e
- return reverse
+    """ 引数に指定された文字列を逆に並べる（listクラスのreverseを使用）。
+
+    Args:
+      arg: （末尾から先頭に向かって）逆に並べる文字列
+    Raise:
+      特になし
+    Return:
+      （末尾から先頭に向かって）逆に並べられた文字列
+    """
+    larg = list(arg)
+    larg.reverse()
+    reverse = ''
+    for e in larg:
+        reverse += e
+    return reverse
+
+#
+# 上記関数を呼び出し
+#
 
 s = "stressed"
 print(reverse(s))
