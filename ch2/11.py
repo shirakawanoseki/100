a2cd8b3e24e4e0ec5fc7b343f854717aa86858bb
Yusuke YONEMURA <swing@os.rim.or.jp>
2019-05-01 18:51:54 +0900

    Create 11.py

---
+++ ch2/11.py
@@ -0,0 +1,20 @@
+
+""" 言語処理100本ノック 2015 第2章: UNIXコマンドの基礎
+11. タブをスペースに置換
+タブ1文字につきスペース1文字に置換せよ．確認にはsedコマンド，trコマンド，もしくはexpandコマンドを用いよ．
+"""
+
+import sys
+import fileinput
+
+with fileinput.input(files=(sys.argv[1])) as input:
+    for line in input:
+        listed_line = list(line)
+        joined_line = []
+        for x in listed_line:
+            if x == '\t':
+                joined_line.append(' ')
+            else:
+                joined_line.append(x)
+        print(''.join(joined_line), end="")
+
