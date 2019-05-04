20aca7d8d8c699f0480c2faf0c0e39658597e03e
Yusuke YONEMURA <swing@os.rim.or.jp>
2019-05-04 12:56:41 +0900

    Create 18.py

---
+++ ch2/18.py
@@ -0,0 +1,29 @@
+
+""" 言語処理100本ノック 2015 第2章: UNIXコマンドの基礎
+18. 各行を3コラム目の数値の降順にソート
+各行を3コラム目の数値の逆順で整列せよ（注意: 各行の内容は変更せずに並び替えよ）．確認にはsortコマンドを用いよ
+（この問題はコマンドで実行した時の結果と合わなくてもよい）．
+"""
+
+#
+# 以下のようにsortコマンドを使用して確認
+# sort -k 2 hightemp.txt
+#
+
+import sys
+import fileinput
+
+with fileinput.input(files=(sys.argv[1])) as input:
+    list_of_list = []
+    for line in input:
+        splited_line = line.split('\t')
+        list_of_list.append(splited_line)
+
+    sorted(list_of_list, key=lambda hightemp: hightemp[2],reverse=True) 
+    list_of_list.reverse()
+
+    for element in list_of_list:
+        print("{0}\t{1}\t{2}\t{3}".format(element[0],element[1],element[2],element[3],), end="")
+    
+
+
