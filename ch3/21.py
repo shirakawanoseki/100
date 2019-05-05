942b3363bdced537afa539118663a062807b8b9f
Yusuke YONEMURA <swing@os.rim.or.jp>
2019-05-05 14:33:02 +0900

    Create 21.py

---
+++ ch3/21.py
@@ -0,0 +1,17 @@
+""" 言語処理100本ノック 2015 第3章: 正規表現
+21. カテゴリ名を含む行を抽出
+記事中でカテゴリ名を宣言している行を抽出せよ．
+"""
+
+import sys
+import json
+import gzip
+import re
+
+with gzip.open(sys.argv[1], "rt") as f:
+    for data_line in f:
+        js = json.loads(data_line)
+        splited_json = js["text"].split('\n')
+        for line in splited_json:
+            if re.match(r'^\[\[Category:(.*)\]\]$', line):
+                print(line)
