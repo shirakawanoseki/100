c6a8d312fcb23bd693f94646bbd1ac958224ad18
Yusuke YONEMURA <detomasomodena@gmail.com>
2019-05-12 12:41:05 +0900

    Create 35.py

---
+++ ch4/35.py
@@ -0,0 +1,103 @@
+
+""" 言語処理100本ノック 2015 第4章: 形態素解析
+35. 名詞の連接
+名詞の連接（連続して出現する名詞）を最長一致で抽出せよ．
+"""
+
+import re
+import sys
+import MeCab
+
+def parse_input_file():
+    """ 「我が輩は猫である」を形態素解析して結果を'neko.txt.mecab'に保存
+
+    Args:
+    　特になし
+    Raise:
+      特になし
+    Return:
+      特になし
+    Note:
+    　入力ファイルはコマンドライン引数より指定
+    """
+    with open(sys.argv[1], mode='r') as input_file, open('neko.txt.mecab', mode='w') as output_file:
+        #入力ファイルの読み込み
+        read_data = input_file.read()
+        #MeCabで形態素解析を行い、結果を出力ファイルに保存
+        mecab = MeCab.Tagger()
+        output_file.write(mecab.parse(read_data))
+
+def build_map_list():
+    """ 「我が輩は猫である」の形態素解析結果のマップリストを作成して返却
+
+    Args:
+    　特になし
+    Raise:
+      特になし
+    Return:
+      形態素解析結果のマップリスト
+    Note:
+    　MeCabのバージョンによっては出力フォーマットが変わるかも。。
+    """
+    master_map_list = []
+    with open('neko.txt.mecab', mode='r') as input_file:
+        #マップのリスト
+        map_list = []
+        for line in input_file.readlines():
+
+            #入力ファイルの終端に達したら(EOSに達したら)ループを抜ける
+            if re.match(r'^EOS$',line) or not line:
+                break
+
+            #フォーマットに従って文を分解
+            split_by_tab = line.split('\t')
+            split_by_comma = split_by_tab[1].split(',')
+
+            #句点に達したら文末と判定
+            if split_by_comma[1] == '句点':
+                master_map_list.append(map_list)
+                map_list = []
+                continue
+
+            #形態素のマップを作成
+            elements = {
+                'surface':split_by_tab[0],
+                'base':split_by_comma[6],
+                'pos':split_by_comma[0],
+                'pos1':split_by_comma[1],
+            }
+
+            map_list.append(elements)
+
+    return master_map_list
+
+if __name__ == "__main__":
+
+    #形態素解析の実行と解析結果の保存
+    parse_input_file()
+    #形態素解析結果の読み込み
+    master_map_list = build_map_list()
+
+    #dicの値は殻空文字列で初期化しておく
+    element_dic_prev = {'surface':'', 'base':'', 'pos':'','pos1':''}
+
+    for master_map in master_map_list:
+        elements_dic_nouns = []
+        for element_dic in master_map:
+            #実行初回、もしくは名詞の連続が途切れた場合
+            if len(elements_dic_nouns) == 0 and element_dic['pos'] == '名詞':
+                elements_dic_nouns.append(element_dic)
+                element_dic_prev = element_dic
+            #名詞が連続する場合
+            elif element_dic['pos'] == '名詞' and element_dic_prev['pos'] == '名詞':
+                elements_dic_nouns.append(element_dic)
+                element_dic_prev = element_dic
+            #名詞が単発で現れて連続しない場合
+            elif element_dic['pos'] != '名詞' and 1 == len(elements_dic_nouns):
+                elements_dic_nouns = []
+            #名詞の連続が途切れた場合
+            elif element_dic['pos'] != '名詞' and 2 <= len(elements_dic_nouns):
+                for nouns in elements_dic_nouns:
+                    print(nouns['surface'],end="")
+                print('')
+                elements_dic_nouns = []
